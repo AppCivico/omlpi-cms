7e823b402d84ed35f976f9936316fecccfe1b6cc
eduruiz <contato@eduruiz.com.br>
2020-01-20 19:37:36 +0000

    WIP

--- api/artigo/controllers/Artigo.js
+++ api/artigo/controllers/Artigo.js
@@ -12,12 +12,13 @@ module.exports = {
   byTag: async ctx => {
     // let entities;
     // if (ctx.query._q) {
-    //   entities = await strapi.services.artigos.search(ctx.query);
+    //   entities = await strapi.services.artigo.search(ctx.query);
     // } else {
-    //   entities = await strapi.services.artigos.find(ctx.query);
+    //   entities = await strapi.services.artigo.find(ctx.query);
     // }
+    const tag = ctx.params.tag;
 
-    return ctx.tags;
+    return tag;
     return entities.map(entity =>
       sanitizeEntity(entity, { model: strapi.models.restaurant })
     );
