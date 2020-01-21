bc3a57c424580d4d15a73eefad6bf78403bced97
edu ruiz <contato@duruiz.com.br>
2020-01-21 19:46:26 -0300

    WIP

--- api/artigo/controllers/Artigo.js
+++ api/artigo/controllers/Artigo.js
@@ -16,11 +16,54 @@ module.exports = {
     // } else {
     //   entities = await strapi.services.artigo.find(ctx.query);
     // }
-    const tag = ctx.params.tag;
+    // const tag = ctx.params.tag;
 
-    return tag;
-    return entities.map(entity =>
-      sanitizeEntity(entity, { model: strapi.models.restaurant })
-    );
+    // let entities = strapi
+    //   .query('artigo')
+    //   .search({ _q: 'tag1', _limit: 100, _sort: 'created_at:asc' });
+    //
+    const result = await strapi
+      .query('artigo')
+      .model.query(qb => {
+        // qb.where('name', '=', 'tag1');
+      })
+      .fetch({withRelated: ['artigo.tags']});
+
+    const fields = result.toJSON();
+
+    return fields;
+
+    // let entities = [];
+    // entities = await contentManagerService.fetchAll(
+    //   ctx.params,
+    //   ctx.request.query
+    // );
+    // return strapi.services.artigo.fetchAll(ctx.query);
+    // return entities;
+    // return entities.map(entity =>
+    //   sanitizeEntity(entity, { model: strapi.models.restaurant })
+    // );
+
+
+    /**
+     * Retrieve records.
+     *
+     * @return {Array}
+     */
+    // let entities;
+    // currentUser = await ctx.state.user;
+    //
+    // if (ctx.query._q) {
+    //   entities = await strapi.services.devices.search(ctx.query);
+    //   //console.log(entities);
+    // } else {
+    //   entities = await strapi.services.devices.find({ organization: currentUser.organization });
+    //   //console.log(ctx.query);
+    //   //console.log(currentUser.organization);
+    // }
+    // return entities.map(entity =>
+    //   sanitizeEntity(entity, { model: strapi.models.devices })
+    // );
+
   },
 };
--- api/artigo/services/Artigo.js
+++ api/artigo/services/Artigo.js
@@ -5,4 +5,22 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  fetchAll: (params) => {
+    // Convert `params` object to filters compatible with Mongo.
+    const filters = strapi.utils.models.convertParams('artigo', params);
+    // Select field to populate.
+    const populate = Artigo.associations
+      .filter(ast => ast.autoPopulate !== false)
+      .map(ast => ast.alias)
+      .join(' ');
+
+    return Artigo
+      .find()
+      .where(filters.where)
+      .sort(filters.sort)
+      .skip(filters.start)
+      .limit(filters.limit)
+      .populate(populate);
+  },
+};
