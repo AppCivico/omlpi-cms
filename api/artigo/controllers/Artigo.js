ce6bd30edfa9967df1efa7c0c6aa6804ac138e43
eduruiz <contato@eduruiz.com.br>
2020-01-22 13:37:05 +0000

    Cleanup

--- api/artigo/controllers/Artigo.js
+++ api/artigo/controllers/Artigo.js
@@ -10,60 +10,15 @@ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 module.exports = {
   // GET /hello
   byTag: async ctx => {
-    // let entities;
-    // if (ctx.query._q) {
-    //   entities = await strapi.services.artigo.search(ctx.query);
-    // } else {
-    //   entities = await strapi.services.artigo.find(ctx.query);
-    // }
-    // const tag = ctx.params.tag;
-
-    // let entities = strapi
-    //   .query('artigo')
-    //   .search({ _q: 'tag1', _limit: 100, _sort: 'created_at:asc' });
-    //
     const result = await strapi
       .query('artigo')
       .model.query(qb => {
-        // qb.where('name', '=', 'tag1');
+        qb.where('id', '=', '1');
       })
-      .fetch({withRelated: ['artigo.tags']});
+      .fetch();
 
     const fields = result.toJSON();
 
     return fields;
-
-    // let entities = [];
-    // entities = await contentManagerService.fetchAll(
-    //   ctx.params,
-    //   ctx.request.query
-    // );
-    // return strapi.services.artigo.fetchAll(ctx.query);
-    // return entities;
-    // return entities.map(entity =>
-    //   sanitizeEntity(entity, { model: strapi.models.restaurant })
-    // );
-
-
-    /**
-     * Retrieve records.
-     *
-     * @return {Array}
-     */
-    // let entities;
-    // currentUser = await ctx.state.user;
-    //
-    // if (ctx.query._q) {
-    //   entities = await strapi.services.devices.search(ctx.query);
-    //   //console.log(entities);
-    // } else {
-    //   entities = await strapi.services.devices.find({ organization: currentUser.organization });
-    //   //console.log(ctx.query);
-    //   //console.log(currentUser.organization);
-    // }
-    // return entities.map(entity =>
-    //   sanitizeEntity(entity, { model: strapi.models.devices })
-    // );
-
   },
 };
