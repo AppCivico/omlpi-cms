7e9c730fc7b40e1fe88e59f68303af95b7ce049a
Junior Moraes <juniorfvox@gmail.com>
2020-01-22 16:08:13 -0300

    Add endpoint to search articles by tag name

--- api/artigo/controllers/Artigo.js
+++ api/artigo/controllers/Artigo.js
@@ -1,24 +1,21 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
-
-const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
-
 module.exports = {
-  // GET /hello
-  byTag: async ctx => {
+  byTag: async (ctx) => {
+
     const result = await strapi
       .query('artigo')
       .model.query(qb => {
-        qb.where('id', '=', '1');
+        qb.join('artigos__tags', 'artigos.id', 'artigos__tags.artigo_id');
+        qb.join('tags', 'tags.id', 'artigos__tags.tag_id');
+
+        const tagName = ctx.params.tag;
+        qb.where('tags.name', '=', tagName);
       })
-      .fetch();
+      .fetchAll();
 
-    const fields = result.toJSON();
-
-    return fields;
+    return result
+      .toJSON()
+      .map(({tags, ...k}) => k)
   },
 };
