678f712384692aa5be8ea2d9abc6b9bd915b0be6
Sobral <robson@oficiodesign.com>
2022-10-05 18:52:01 -0300

    Avoid restarting server on database changes

--- config/server.js
+++ config/server.js
@@ -5,5 +5,14 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6265ef0ec55a648f1b957e8bf99f0295'),
     },
+
+    /**
+     * @link https://github.com/strapi/strapi/issues/8848#issuecomment-827664123
+     */
+    watchIgnoreFiles: [
+      '**/info.log',
+      '**/error.log',
+      '/srv/app/data/**',
+    ],
   },
 });
